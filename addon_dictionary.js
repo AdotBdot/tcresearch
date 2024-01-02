581d15a8004988f89ecd64884d7053f28436aa9e
Adam Buchowski <42146911+AdotBdot@users.noreply.github.com>
2024-01-02 20:34:38 +0100

    Update addon_dictionary.js

--- addon_dictionary.js
+++ addon_dictionary.js
@@ -3,7 +3,7 @@ var addon_dictionary = {
 	"name": "Avaritia",
 	"aspects":["apocalypse"],
 	"combinations":{
-		"apocalypse": ["greed", "alien"]
+		"apocalypse": ["greed", "alienis"]
 		} 
 	},
 "fm": {
