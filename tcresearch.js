c6ee31053743942963fb6fc7b41e408498befd6c
Adam Buchowski <adam.buchowski@proton.me>
2026-03-01 15:46:56 +0000

    Changed latest version to 6.1.BETA26

--- tcresearch.js
+++ tcresearch.js
@@ -1,6 +1,6 @@
 $(function()
 {
-	var latest_version = "5.1.3";
+	var latest_version = "6.1.BETA26";
 	$.each(version_dictionary, function(key, version){
 		$("#version").append("<option value=" + key + ">" + key + "</option>");
 	});
